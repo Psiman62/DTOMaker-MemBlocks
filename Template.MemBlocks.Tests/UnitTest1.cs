c63e994dc492e8d108ea261e4f03ff59e47bb26f
Psiman62 <simon_dudley@msn.com>
2024-10-16 23:18:00 +1000

    started template tests

---
+++ Template.MemBlocks.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+
+namespace Template.MemBlocks.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var orig = new T_DomainName_.MemBlocks.T_EntityName_();
+            orig.T_MemberName_ = 123.456D;
+            orig.Freeze();
+
+            var buffer = orig.Block;
+
+            var copy = new T_DomainName_.MemBlocks.T_EntityName_(buffer);
+            copy.IsFrozen().Should().BeTrue();
+            copy.T_MemberName_.Should().Be(orig.T_MemberName_);
+        }
+    }
+}
